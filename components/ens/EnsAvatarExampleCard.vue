d56a7ab6c52b55d3a0269555dc14046d4bda83a0
conradocanas <conradocanas@gmail.com>
2023-08-21 13:52:23 -0300

    [ENS] Avatar example moved to component

---
+++ components/ens/EnsAvatarExampleCard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { useEnsAvatar } from 'use-wagmi'
+
+const ensExampleAvatar = ref(['jxom.eth', 'domico.eth', 'awkweb.eth'])
+const selectedEnsAvatar = ref('')
+
+const { data: ensAvatar, isLoading: loadingAvatar } = useEnsAvatar({
+  name: selectedEnsAvatar,
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">
+        ENS Avatar Example
+      </h5>
+      <div class="row g-0">
+        <div class="col">
+          <select
+            v-model="selectedEnsAvatar" class="form-select form-select-lg"
+            aria-label="Default select example"
+          >
+            <option value="" selected>
+              Pick an avatar
+            </option>
+            <option v-for="item in ensExampleAvatar" :key="item" :value="item">
+              {{ item }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="mt-4">
+        <p v-if="loadingAvatar">
+          Loading avatar...
+        </p>
+        <img v-else-if="ensAvatar" style="max-width: 150px;" :src="ensAvatar" alt="">
+        <p v-else-if="selectedEnsAvatar">
+          ENS Not found
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-import { useEnsAvatar } from 'use-wagmi'
-
-const ensExampleAvatar = ref(['jxom.eth', 'domico.eth', 'awkweb.eth'])
-const selectedEnsAvatar = ref('')
-
-const { data: ensAvatar, isLoading: loadingAvatar } = useEnsAvatar({
-  name: selectedEnsAvatar,
-})
-</script>
-
 <template>
   <main>
     <div class="container py-4">
@@ -24,37 +13,7 @@ const { data: ensAvatar, isLoading: loadingAvatar } = useEnsAvatar({
           </div>
         </div>
         <div class="col-12 col-md-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                ENS Avatar Example
-              </h5>
-              <div class="row g-0">
-                <div class="col">
-                  <select
-                    v-model="selectedEnsAvatar" class="form-select form-select-lg"
-                    aria-label="Default select example"
-                  >
-                    <option value="" selected>
-                      Pick an avatar
-                    </option>
-                    <option v-for="item in ensExampleAvatar" :key="item" :value="item">
-                      {{ item }}
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <div class="mt-4">
-                <p v-if="loadingAvatar">
-                  Loading avatar...
-                </p>
-                <img v-else-if="ensAvatar" style="max-width: 150px;" :src="ensAvatar" alt="">
-                <p v-else-if="selectedEnsAvatar">
-                  ENS Not found
-                </p>
-              </div>
-            </div>
-          </div>
+          <EnsAvatarExampleCard />
         </div>
       </div>
     </div>
