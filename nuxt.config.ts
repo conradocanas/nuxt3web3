50af7f065bce26c24ce6a8dd1a0bde5b3e1663e8
conradocanas <conradocanas@gmail.com>
2023-08-21 05:04:00 -0300

    [NUXT] App head - title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Nuxt3 - Wagmi/Web3Modal',
+    },
+  },
   css: ['bootstrap/dist/css/bootstrap.min.css'],
   devtools: { enabled: true },
   runtimeConfig: {
