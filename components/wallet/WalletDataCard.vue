b04f1eb908eefe561a5a1d6ad930a8d789363615
conradocanas <conradocanas@gmail.com>
2023-08-21 13:42:01 -0300

    [UI] Layout updated with responsive

---
+++ components/wallet/WalletDataCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useAccount, useBalance } from 'use-wagmi'
+
+const { address, status } = useAccount()
+const { data: balance, isLoading: isLoadingBalance } = useBalance({
+  address,
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">
+        Wallet Data
+      </h5>
+      <div>
+        <p>Status: <span :class="status === 'connected' ? 'text-success' : ''">{{ status }}</span></p>
+        <p>Address: {{ address }}</p>
+      </div>
+      <div>
+        <p>
+          Balance: <span v-if="isLoadingBalance">loading...</span> <span v-else>{{ balance?.formatted }} {{
+            balance?.symbol }}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-import { useAccount, useBalance, useEnsAvatar } from 'use-wagmi'
-
-const { address, status } = useAccount()
-const { data: balance, isLoading: isLoadingBalance } = useBalance({
-  address,
-})
+import { useEnsAvatar } from 'use-wagmi'
 
 const ensExampleAvatar = ref(['jxom.eth', 'domico.eth', 'awkweb.eth'])
 const selectedEnsAvatar = ref('')
@@ -18,23 +13,17 @@ const { data: ensAvatar, isLoading: loadingAvatar } = useEnsAvatar({
   <main>
     <div class="container py-4">
       <div class="row g-4">
-        <div class="col">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                Wallet Data
-              </h5>
-              <div>
-                <p>Status: <span :class="status === 'connected' ? 'text-success' : ''">{{ status }}</span></p>
-                <p>Address: {{ address }}</p>
-              </div>
-              <div>
-                <p>Balance: <span v-if="isLoadingBalance">loading...</span> <span v-else>{{ balance?.formatted }} {{ balance?.symbol }}</span></p>
-              </div>
+        <div class="col-12 col-md-6">
+          <div class="row g-4">
+            <div class="col-12">
+              <WalletDataCard />
+            </div>
+            <div class="col-12">
+              <TransactionsExampleCard />
             </div>
           </div>
         </div>
-        <div class="col-6">
+        <div class="col-12 col-md-6">
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">
@@ -42,7 +31,10 @@ const { data: ensAvatar, isLoading: loadingAvatar } = useEnsAvatar({
               </h5>
               <div class="row g-0">
                 <div class="col">
-                  <select v-model="selectedEnsAvatar" class="form-select form-select-lg" aria-label="Default select example">
+                  <select
+                    v-model="selectedEnsAvatar" class="form-select form-select-lg"
+                    aria-label="Default select example"
+                  >
                     <option value="" selected>
                       Pick an avatar
                     </option>
@@ -63,9 +55,6 @@ const { data: ensAvatar, isLoading: loadingAvatar } = useEnsAvatar({
               </div>
             </div>
           </div>
-        </div>
-        <div class="col-6">
-          <TransactionsExampleCard />
         </div>
       </div>
     </div>
